2b1815e9e06dcc8d1e263d2e26cd32a48d195513
dindonstudio <78362187+dindonstudio@users.noreply.github.com>
2023-04-10 12:53:47 +0200

    yy

--- components/imageGallery.tsx
+++ components/imageGallery.tsx
@@ -233,10 +233,10 @@ $(".galleryImage").on("mouseup", "img", function (event) {
                        width={1200}
                       height={1800}
                       quality={85}
-                      priority={indexSlide < 2}
+                      // priority={indexSlide < 2}
                       draggable={false}
-                      blurDataURL={indexSlide > 2 ? image.img.blurDataURL : undefined} // Use the base64 URL from the matchingSeries objects only if indexSlide > 2
-                      placeholder={indexSlide > 2 ? "blur" : undefined}
+                      blurDataURL={ image.img.blurDataURL} // Use the base64 URL from the matchingSeries objects only if indexSlide > 2
+                        placeholder="blur"
                       alt="gallery image"
                     />
                     );
