9385f1831cd5a33d2322c4a2232ee0f317023650
dindonstudio <78362187+dindonstudio@users.noreply.github.com>
2023-06-16 10:44:57 +0200

    test : only 6 slides

--- components/imageGallery.tsx
+++ components/imageGallery.tsx
@@ -191,57 +191,58 @@ $(".galleryImage").on("mouseup", "img", function (event) {
 
   return (
     <div className="   md:pt-72 grid pt-40 ">
-      {slides.map((slide, indexSlide) => {
-        const matchingSerie = series.find((serie) => serie._id === slide._ref);
-        if (matchingSerie) {
-          return (
-            <div
-              className={`customRowspan  galleryImage new animatedScale relative grid transitionScaleUp z-10 ${
-                clickedIndex === indexSlide && isMaxRow ? "max-row" : "min-row"
-              } galleryOrigin`}
-              onMouseEnter={Drag}
-              onClick={(event) => handleMaxRowToggle(event, indexSlide)}
-              key={indexSlide}
-            >
-              {" "}
-              <div
-                data-aos="scaleY"
-                data-aos-id="super-duper"
-                data-aos-once="true"
-                className="customRowspanSmall transitionScaleUp "
-                key={indexSlide}
-              >
-                <div className="md:pb-6 flex cursor-grab flex-nowrap h-full overflow-x-auto gap-2 hideScrollBar pb-4 galleryOrigin transitionScaleUp imageContainer passive ">
-                  {matchingSerie.images.map((image, index) => {
-                    return (
-                      <Image
-                        key={index}
-                        className={`flex-shrink-0 w-auto h-full  ${
-                          index === 0 ? "md:DesktopPaddingleft pl-8" : ""
-                        }`}
-                        src={urlFor(image.asset).url()}
-                        width={1200}
-                        height={1800}
-                        quality={85}
-                        
-                        draggable={false}
-                        alt="gallery image"
-                      />
-                    );
-                  })}
-                </div>
-                <div className="md:flex md:pl-0 md:DesktopPaddingleft pl-8 title transitionScaleUp titleOrigin">
-                  <h4 className="fontSize">{matchingSerie.title}</h4>
-                  <h4 className=" md:pl-4 fontSize">{matchingSerie.date}</h4>
-                </div>
-              </div>
-              <div className="spaccer row-span-2"></div>
-            </div>
-          );
-        } else {
-          <div></div>;
-        }
-      })}
+  {slides.slice(0, 6).map((slide, indexSlide) => {
+  const matchingSerie = series.find((serie) => serie._id === slide._ref);
+  if (matchingSerie) {
+    return (
+      <div
+        className={`customRowspan  galleryImage new animatedScale relative grid transitionScaleUp z-10 ${
+          clickedIndex === indexSlide && isMaxRow ? "max-row" : "min-row"
+        } galleryOrigin`}
+        onMouseEnter={Drag}
+        onClick={(event) => handleMaxRowToggle(event, indexSlide)}
+        key={indexSlide}
+      >
+        {" "}
+        <div
+          data-aos="scaleY"
+          data-aos-id="super-duper"
+          data-aos-once="true"
+          className="customRowspanSmall transitionScaleUp "
+          key={indexSlide}
+        >
+          <div className="md:pb-6 flex cursor-grab flex-nowrap h-full overflow-x-auto gap-2 hideScrollBar pb-4 galleryOrigin transitionScaleUp imageContainer passive ">
+            {matchingSerie.images.map((image, index) => {
+              return (
+                <Image
+                  key={index}
+                  className={`flex-shrink-0 w-auto h-full  ${
+                    index === 0 ? "md:DesktopPaddingleft pl-8" : ""
+                  }`}
+                  src={urlFor(image.asset).url()}
+                  width={1200}
+                  height={1800}
+                  quality={85}
+                  priority
+                  draggable={false}
+                  alt="gallery image"
+                />
+              );
+            })}
+          </div>
+          <div className="md:flex md:pl-0 md:DesktopPaddingleft pl-8 title transitionScaleUp titleOrigin">
+            <h4 className="fontSize">{matchingSerie.title}</h4>
+            <h4 className=" md:pl-4 fontSize">{matchingSerie.date}</h4>
+          </div>
+        </div>
+        <div className="spaccer row-span-2"></div>
+      </div>
+    );
+  } else {
+    return <div key={indexSlide}></div>;
+  }
+})}
+
     </div>
   );
 };
