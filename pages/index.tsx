03e1cd798c8fe4ac8c226df10b0dedee4c15c753
dindonstudio <78362187+dindonstudio@users.noreply.github.com>
2023-06-19 15:30:25 +0200

    test preload

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Key, ReactNode, useState } from "react";
 import HeaderNav from "@/components/headerNav";
 import ImageHeader from "@/components/imageHeader";
 import Contact from "@/components/contact";
+import Image from "next/image";
 import urlFor from "@/lib/urlFor";
 import Header from "@/components/header";
 import { useEffect } from "react";
@@ -42,24 +43,39 @@ export default function HomePage({ header, menu, page }: { header: any; menu: an
         <title>Paul Louisor </title>
         <meta property="og:title" content="Paul Louisor" key="title" />
 
-        {page.map((serie: { slides: any[]; }) =>
-    serie.slides.slice(0, 2).map((slide: { images: any[]; }) =>
-      slide.images.map((image: { asset: any; }, index: Key | null | undefined) => (
-        <link
-          key={index}
-          rel="preload"
-          as="image"
-          href={urlFor(image.asset).url()}
-        />
-      ))
-    )
-  )}
+
 
         {/* <meta property="og:description" content={header.description} key="title" /> */}
       </Head>
       <HeaderNav header={header} menu={menu} />
       <ImageHeader menu={menu} />
       <Contact header={header} />
+      <div className="hidden">
+  
+{page.map((serie: { slides: any[]; }) =>
+
+    serie.slides.slice(0, 3).map((slide: { images: any[]; }) =>
+      slide.images.map((image: { asset: any; }, index: Key | null | undefined) => (
+      
+        <Image
+        key={index}
+        className={`flex-shrink-0 w-auto h-full  ${
+          index === 0 ? "md:DesktopPaddingleft pl-8" : ""
+        }`}
+        src={urlFor(image.asset).url()}
+        width={1200}
+        height={1800}
+        quality={85}
+        priority={true}
+        draggable={false}
+        alt="gallery image"
+      />
+      
+      ))
+    )
+  )}
+  {console.log(page.serie)}
+</div>
     </div>
   );
 }
@@ -74,7 +90,7 @@ export async function getStaticProps() {
   // Modify the series query to limit the results to 2 series, including all images
   const queryPage = groq`*[_type == 'pages'][0...3]{..., "slides": slides[]->{..., "images": images}}`;
     const dataPage = await client.fetch(queryPage);
-
+console.log(dataPage)
   if (data && data.length > 0) {
     return {
       props: {
