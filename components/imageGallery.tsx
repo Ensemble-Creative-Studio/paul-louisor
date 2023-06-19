26f9f9dca46e0d8a0d3e9c5df866379a30373e81
dindonstudio <78362187+dindonstudio@users.noreply.github.com>
2023-06-19 13:08:04 +0200

    test less image

--- components/imageGallery.tsx
+++ components/imageGallery.tsx
@@ -9,6 +9,7 @@ import AOS from "aos";
 import $ from "jquery";
 
 import Link from "next/link";
+
 interface Slide {
   images: any;
   _key: string;
@@ -54,96 +55,80 @@ const ImageGallery = ({ slides, series }: ImageGalleryProps) => {
       const galleryImages = document.querySelectorAll(".imageContainer");
       let dragging = false;
 
-$(".galleryImage").on("mousedown", "img", function () {
-    dragging = false;
-});
+      $(".galleryImage").on("mousedown", "img", function () {
+        dragging = false;
+      });
 
-$(".galleryImage").on("mousemove", "img", function () {
-    dragging = true;
-});
+      $(".galleryImage").on("mousemove", "img", function () {
+        dragging = true;
+      });
 
-$(".galleryImage").on("mouseup", "img", function (event) {
-    if (!dragging) { // Only run the code if the user is not dragging the container
-      const container = $(this).parent(".imageContainer");
-      const galleryImage = container.parents(".galleryImage");
-      
-      // Remove max-row class from other gallery images
-      
-      // $(".galleryImage.max-row").not(galleryImage).removeClass("max-row");
-      
-      const isMaxRow = galleryImage.hasClass("max-row");
-      if (isMaxRow){
-        setTimeout(() => {
-          galleryImage.toggleClass("max-row");
-        }, 600);
-        setTimeout(() => {
-          const imagePos = $(this).position();
-          if (!container) {
-              return; // exit the function if container is undefined
-          }
-          const containerPos = container[0].offsetLeft;
-          const imageLeft = isMaxRow ? 0 : imagePos.left - containerPos + container[0].scrollLeft;
-          container.animate(
-              {
+      $(".galleryImage").on("mouseup", "img", function (event) {
+        if (!dragging) {
+          // Only run the code if the user is not dragging the container
+          const container = $(this).parent(".imageContainer");
+          const galleryImage = container.parents(".galleryImage");
+
+          const isMaxRow = galleryImage.hasClass("max-row");
+          if (isMaxRow) {
+            setTimeout(() => {
+              galleryImage.toggleClass("max-row");
+            }, 600);
+            setTimeout(() => {
+              const imagePos = $(this).position();
+              if (!container) {
+                return; // exit the function if container is undefined
+              }
+              const containerPos = container[0].offsetLeft;
+              const imageLeft =
+                isMaxRow ? 0 : imagePos.left - containerPos + container[0].scrollLeft;
+              container.animate(
+                {
                   scrollLeft: imageLeft,
-              },
-              "slow"
-          );
-
-
-            // const rect = event.target.getBoundingClientRect();
-            // const bgWhite = document.querySelector(".galleryContainer") as HTMLElement;
-            // if (event.target.nextElementSibling) {
-            //     const top = bgWhite.scrollTop + rect.top - window.innerHeight * 0.105;
-            //     bgWhite.scrollTo({
-            //         top,
-            //         behavior: "smooth",
-            //     });
-            // }
-        }, 0);
-      }
-      else{
-        galleryImage.toggleClass("max-row");
-        setTimeout(() => {
-          const imagePos = $(this).position();
-          if (!container) {
-              return; // exit the function if container is undefined
-          }
-          const containerPos = container[0].offsetLeft;
-          const imageLeft = isMaxRow ? 0 : imagePos.left - containerPos + container[0].scrollLeft;
-          container.animate(
-              {
+                },
+                "slow"
+              );
+            }, 0);
+          } else {
+            galleryImage.toggleClass("max-row");
+            setTimeout(() => {
+              const imagePos = $(this).position();
+              if (!container) {
+                return; // exit the function if container is undefined
+              }
+              const containerPos = container[0].offsetLeft;
+              const imageLeft =
+                isMaxRow ? 0 : imagePos.left - containerPos + container[0].scrollLeft;
+              container.animate(
+                {
                   scrollLeft: imageLeft,
-              },
-              "slow"
-          );
+                },
+                "slow"
+              );
 
-
-            const rect = event.target.getBoundingClientRect();
-            const bgWhite = document.querySelector(".galleryContainer") as HTMLElement;
-            if (event.target.nextElementSibling) {
-                const top = bgWhite.scrollTop + rect.top - window.innerHeight * 0.105;
+              const rect = event.target.getBoundingClientRect();
+              const bgWhite = document.querySelector(
+                ".galleryContainer"
+              ) as HTMLElement;
+              if (event.target.nextElementSibling) {
+                const top =
+                  bgWhite.scrollTop + rect.top - window.innerHeight * 0.105;
                 bgWhite.scrollTo({
-                    top,
-                    behavior: "smooth",
+                  top,
+                  behavior: "smooth",
                 });
-            }
-        }, 600);
-      }
-
-
-
-    }
-});
+              }
+            }, 600);
+          }
+        }
+      });
 
       return () => {
         bgWhite.removeEventListener("scroll", handleScroll);
         galleryImages.forEach((imageContainer) => {
           const images = imageContainer.querySelectorAll("img");
           images.forEach((image, index) => {
-            image.removeEventListener("click", () => {
-
-            });
+            image.removeEventListener("click", () => {});
           });
         });
       };
@@ -158,91 +143,87 @@ $(".galleryImage").on("mouseup", "img", function (event) {
   const [clickedIndex, setClickedIndex] = useState(-1);
 
   const handleMaxRowToggle = (event: any, index: any) => {
-    // console.log('ckk')
-    // // Check if clicked element has class 'passive'
-    // let drag = false;
+    // Check if clicked element has class 'passive'
+    let drag = false;
 
-    // document.addEventListener("mousedown", () => (drag = false));
-    // document.addEventListener("mouseup", () => (drag = true));
-    // document.addEventListener("mousemove", () => (drag = true));
+    document.addEventListener("mousedown", () => (drag = false));
+    document.addEventListener("mouseup", () => (drag = true));
+    document.addEventListener("mousemove", () => (drag = true));
 
-    // if (event.target.classList.contains("passive")) {
-    //   if (clickedIndex === index && drag === false) {
-    //     setIsMaxRow(!isMaxRow);
-    //   } else {
-    //     setClickedIndex(index);
-    //     setIsMaxRow(true);
-    //   }
-    //   setTimeout(() => {
-    //     const rect = event.target.getBoundingClientRect();
-    //     const bgWhite = document.querySelector(
-    //       ".galleryContainer"
-    //     ) as HTMLElement;
-    //     if (event.target.nextElementSibling) {
-    //       const top = bgWhite.scrollTop + rect.top - window.innerHeight * 0.105;
-    //       bgWhite.scrollTo({
-    //         top,
-    //         behavior: "smooth",
-    //       });
-    //     }
-    //   }, 700);
-    // }
+    if (event.target.classList.contains("passive")) {
+      if (clickedIndex === index && drag === false) {
+        setIsMaxRow(!isMaxRow);
+      } else {
+        setClickedIndex(index);
+        setIsMaxRow(true);
+      }
+      setTimeout(() => {
+        const rect = event.target.getBoundingClientRect();
+        const bgWhite = document.querySelector(".galleryContainer") as HTMLElement;
+        if (event.target.nextElementSibling) {
+          const top = bgWhite.scrollTop + rect.top - window.innerHeight * 0.105;
+          bgWhite.scrollTo({
+            top,
+            behavior: "smooth",
+          });
+        }
+      }, 700);
+    }
   };
 
   return (
     <div className="   md:pt-72 grid pt-40 ">
-  {slides.slice(0, 3).map((slide, indexSlide) => {
-  const matchingSerie = series.find((serie) => serie._id === slide._ref);
-  if (matchingSerie) {
-    return (
-      <div
-        className={`customRowspan  galleryImage new animatedScale relative grid transitionScaleUp z-10 ${
-          clickedIndex === indexSlide && isMaxRow ? "max-row" : "min-row"
-        } galleryOrigin`}
-        onMouseEnter={Drag}
-        onClick={(event) => handleMaxRowToggle(event, indexSlide)}
-        key={indexSlide}
-      >
-        {" "}
-        <div
-          data-aos="scaleY"
-          data-aos-id="super-duper"
-          data-aos-once="true"
-          className="customRowspanSmall transitionScaleUp "
-          key={indexSlide}
-        >
-          <div className="md:pb-6 flex cursor-grab flex-nowrap h-full overflow-x-auto gap-2 hideScrollBar pb-4 galleryOrigin transitionScaleUp imageContainer passive ">
-            {matchingSerie.images.map((image, index) => {
-              return (
-                <Image
-                  key={index}
-                  className={`flex-shrink-0 w-auto h-full  ${
-                    index === 0 ? "md:DesktopPaddingleft pl-8" : ""
-                  }`}
-                  src={urlFor(image.asset).url()}
-                  width={1200}
-                  height={1800}
-                  quality={85}
-                  priority
-                  draggable={false}
-                  alt="gallery image"
-                />
-              );
-            })}
-          </div>
-          <div className="md:flex md:pl-0 md:DesktopPaddingleft pl-8 title transitionScaleUp titleOrigin">
-            <h4 className="fontSize">{matchingSerie.title}</h4>
-            <h4 className=" md:pl-4 fontSize">{matchingSerie.date}</h4>
-          </div>
-        </div>
-        <div className="spaccer row-span-2"></div>
-      </div>
-    );
-  } else {
-    return <div key={indexSlide}></div>;
-  }
-})}
+      {slides.map((slide, indexSlide) => {
 
+        const matchingSerie = series.find((serie) => serie._id === slide._ref);
+        if (matchingSerie) {
+          return (
+            <div
+              className={`customRowspan  galleryImage new animatedScale relative grid transitionScaleUp z-10 ${
+                clickedIndex === indexSlide && isMaxRow ? "max-row" : "min-row"
+              } galleryOrigin`}
+              onMouseEnter={Drag}
+              onClick={(event) => handleMaxRowToggle(event, indexSlide)}
+              key={indexSlide}
+            >
+              <div
+                data-aos="scaleY"
+                data-aos-id="super-duper"
+                data-aos-once="true"
+                className="customRowspanSmall transitionScaleUp "
+                key={indexSlide}
+              >
+                <div className="md:pb-6 flex cursor-grab flex-nowrap h-full overflow-x-auto gap-2 hideScrollBar pb-4 galleryOrigin transitionScaleUp imageContainer passive ">
+                  {matchingSerie.images.map((image, index) => {
+                    return (
+                      <Image
+                        key={index}
+                        className={`flex-shrink-0 w-auto h-full  ${
+                          index === 0 ? "md:DesktopPaddingleft pl-8" : ""
+                        }`}
+                        src={urlFor(image.asset).url()}
+                        width={1200}
+                        height={1800}
+                        quality={85}
+                        priority
+                        draggable={false}
+                        alt="gallery image"
+                      />
+                    );
+                  })}
+                </div>
+                <div className="md:flex md:pl-0 md:DesktopPaddingleft pl-8 title transitionScaleUp titleOrigin">
+                  <h4 className="fontSize">{matchingSerie.title}</h4>
+                  <h4 className="md:pl-4 fontSize">{matchingSerie.date}</h4>
+                </div>
+              </div>
+              <div className="spaccer row-span-2"></div>
+            </div>
+          );
+        } else {
+          return <div key={indexSlide}></div>;
+        }
+      })}
     </div>
   );
 };
--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -14,16 +14,20 @@ export default function Page({
   header,
   menu,
   page,
+  matchingSlides,
+  matchingSeries
 }: {
   header: any;
   menu: any;
   page: any;
+  matchingSlides: any[];
+  matchingSeries: any[];
 }) {
   
   const router = useRouter();
   const slug = router.query.slug ?? "";
-  const [matchingSlides, setMatchingSlides] = useState([]);
-  const [matchingSeries, setMatchingSeries] = useState([]);
+  // const [matchingSlides, setMatchingSlides] = useState([]);
+  // const [matchingSeries, setMatchingSeries] = useState([]);
   const [nextPageUrl, setNextPageUrl] = useState("");
   const [nextPageSlug, setNextPageSlug] = useState("");
   const CapitaliseSlug = (slug as string).charAt(0).toUpperCase() + slug.slice(1);
@@ -41,19 +45,23 @@ export default function Page({
     const currentPage = menu?.find(
       (page: { slug: { current: any } }) => page.slug.current === slug
     );
-    const matchingSlides = slug === '/' ? [] : currentPage?.slides ?? [];
-    const matchingSeries = page ?? [];
+    // const matchingSlides = slug === '/' ? [] : currentPage?.slides ?? [];
+    // const matchingSeries = page ?? [];
 
 
     const pageIndex = menu?.findIndex((item: any) => item.slug.current === slug);
     const nextPageIndex = (pageIndex + 1) % menu?.length; // wrap around to first page if at last page
     const nextPageSlug = menu[nextPageIndex].slug.current;
     const nextPageUrl = `/${nextPageSlug}`;
-    setMatchingSlides(matchingSlides);
-    setMatchingSeries(matchingSeries);
+    // const matchingSlidesTemp = slug === '/' ? [] : currentPage?.slides ?? [];
+    // const matchingSeriesTemp = page ?? [];
+  
+    // setMatchingSlides(matchingSlidesTemp);
+    // setMatchingSeries(matchingSeriesTemp);
+ 
     setNextPageUrl(nextPageUrl);
     setNextPageSlug(nextPageSlug);
-  }, []);
+  }, [page, menu]);
   return (
     <div className="bg-white galleryContainer new overflow-hidden h-screen overflow-y-scroll">
      <Head>
@@ -78,22 +86,37 @@ export const getStaticProps = async (context: { params: { slug: any } }) => {
   const { slug } = context.params;
   const query = groq`*[_type == 'siteSettings' ] `;
   const data = await client.fetch(query);
-  const queryMenu = groq`*[_type == 'pages' ]|order(orderRank) `;
+  const queryMenu = groq`*[_type == 'pages' ] { ..., slides[0...3] } | order(orderRank) `;
   const dataMenu = await client.fetch(queryMenu);
-  const queryPage = groq`*[_type == 'series']`;
-  const dataPage = await client.fetch(queryPage);
+  const queryPage = groq`*[_type == 'pages' && slug.current == $slug]{ ..., slides[0...3] }`;
+  const paramsPage = { slug: slug };
+  const dataPage = await client.fetch(queryPage, paramsPage);
+
+  // Find the matching slides and series
+  const matchingSlides = slug === '/' ? [] : dataPage[0].slides ?? [];
+  const matchingSeriesIds = matchingSlides
+    .slice(0, 3) // Get the first three slides
+    .map((slide: { _ref: any; }) => slide._ref); // Extract the _ref values of the slides
+
+  // Fetch the corresponding series based on the _ref values
+  const querySeries = groq`*[_type == 'series' && _id in $seriesIds]`;
+  const paramsSeries = { seriesIds: matchingSeriesIds };
+  const matchingSeries = await client.fetch(querySeries, paramsSeries);
 
   if (data && data.length > 0) {
     return {
       props: {
         header: data[0],
         menu: dataMenu,
-        page: dataPage,
+        page: dataPage[0],
+        matchingSlides,
+        matchingSeries
       },
     };
   }
   return { props: { header: null, dataMenu: null } };
 };
+
 export async function getStaticPaths() {
   const query = groq`*[_type == 'pages'  ]`;
   const data = await client.fetch(query);
