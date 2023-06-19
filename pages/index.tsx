b25c8c7c72fa8dc7d57c3ca6ce281b9d61439b31
dindonstudio <78362187+dindonstudio@users.noreply.github.com>
2023-06-19 11:06:36 +0200

    test preloading image home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,30 @@
 import { groq } from "next-sanity";
 import { client } from "../lib/sanity.client";
-import { ReactNode, useState } from "react";
+import { Key, ReactNode, useState } from "react";
 import HeaderNav from "@/components/headerNav";
 import ImageHeader from "@/components/imageHeader";
 import Contact from "@/components/contact";
+import urlFor from "@/lib/urlFor";
 import Header from "@/components/header";
 import { useEffect } from "react";
 import Head from "next/head";
-import {animateSiteTitle, shouldAnimateSiteTitle, markSiteTitleAsAnimated  } from '../components/utils/intro'
-interface PageProps {
+import {
+  animateSiteTitle,
+  shouldAnimateSiteTitle,
+  markSiteTitleAsAnimated,
+} from "../components/utils/intro";
+interface PageProps {}
 
-}
-
-export default function HomePage({ header, menu }: { header: any, menu: any }) {
+export default function HomePage({ header, menu, page }: { header: any; menu: any, page:any }) {
   const shouldAnimate = shouldAnimateSiteTitle();
 
   useEffect(() => {
-    const homeGridColRow = document.querySelector('.homeGridColRow') as HTMLElement;
+
+    const homeGridColRow = document.querySelector(
+      ".homeGridColRow"
+    ) as HTMLElement;
     if (homeGridColRow) {
-      homeGridColRow.style.opacity = '1';
+      homeGridColRow.style.opacity = "1";
     }
     if (shouldAnimate) {
       animateSiteTitle();
@@ -26,22 +32,34 @@ export default function HomePage({ header, menu }: { header: any, menu: any }) {
     }
     return () => {
       if (homeGridColRow) {
-        homeGridColRow.style.opacity = '';
+        homeGridColRow.style.opacity = "";
       }
     };
   }, [shouldAnimate]);
   return (
-    
     <div className="homeGridColRow grid h-svh opacity-0 ">
-    <Head>
+      <Head>
         <title>Paul Louisor </title>
         <meta property="og:title" content="Paul Louisor" key="title" />
+
+        {page.map((serie: { slides: any[]; }) =>
+    serie.slides.slice(0, 2).map((slide: { images: any[]; }) =>
+      slide.images.map((image: { asset: any; }, index: Key | null | undefined) => (
+        <link
+          key={index}
+          rel="preload"
+          as="image"
+          href={urlFor(image.asset).url()}
+        />
+      ))
+    )
+  )}
+
         {/* <meta property="og:description" content={header.description} key="title" /> */}
       </Head>
-        <HeaderNav  header={header} menu = {menu}/>
-        <ImageHeader menu = {menu}/>
-        <Contact header={header}/>
-        
+      <HeaderNav header={header} menu={menu} />
+      <ImageHeader menu={menu} />
+      <Contact header={header} />
     </div>
   );
 }
@@ -49,16 +67,24 @@ export default function HomePage({ header, menu }: { header: any, menu: any }) {
 export async function getStaticProps() {
   const query = groq`*[_type == 'siteSettings' ] `;
   const data = await client.fetch(query);
+
   const queryMenu = groq`*[_type == 'pages' ] |order(orderRank) `;
   const dataMenu = await client.fetch(queryMenu);
+
+  // Modify the series query to limit the results to 2 series, including all images
+  const queryPage = groq`*[_type == 'pages'][0...3]{..., "slides": slides[]->{..., "images": images}}`;
+    const dataPage = await client.fetch(queryPage);
+console.log(dataPage)
   if (data && data.length > 0) {
     return {
       props: {
         header: data[0],
-        menu: dataMenu
+        menu: dataMenu,
+        page: dataPage,
       },
     };
   }
-  return { props: { header: null, dataMenu: null } };
+  
+  return { props: { header: null, dataMenu: null, dataPage: null } };
 }
 
